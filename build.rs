03a062c52f36d8cc260dab6115df1f788ca657e3
Christian <christian@chfi.se>
2020-10-25 00:36:52 -0500

    Use bindgen to generate bindings to WFA

--- build.rs
+++ build.rs
@@ -1,4 +1,31 @@
+use std::{env, path::PathBuf};
+
 fn main() {
     println!("cargo:rustc-link-search=native=libwfa");
-    println!("cargo:rustc-link-lib=static=wfa");
+    // println!("cargo:rustc-link-lib=static=wfa");
+    println!("cargo:rustc-link-lib=wfa");
+
+    let bindings = bindgen::Builder::default()
+        .clang_arg("-IWFA")
+        // .clang_arg("-includeWFA/")
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .blacklist_item("FP_NAN")
+        .blacklist_item("FP_INFINITE")
+        .blacklist_item("FP_ZERO")
+        .blacklist_item("FP_SUBNORMAL")
+        .blacklist_item("FP_NORMAL")
+        // .blacklist_type("FP_NAN")
+        // .blacklist_type("FP_INFINITE")
+        // .blacklist_type("FP_ZERO")
+        // .blacklist_type("FP_SUBNORMAL")
+        // .blacklist_type("FP_NORMAL")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-use libc::size_t;
+use libc::{c_int, size_t};
 
+/*
 #[repr(C)]
 pub struct MMAllocator {
-    _private: [u8; 0],
+    // _private: [u8; 0],
+    _private: (),
 }
 
 extern "C" {
     fn mm_allocator_new(segment_size: u64) -> *const MMAllocator;
 }
+*/
 
 fn main() {
     println!("making allocator");
-    unsafe {
-        let alloc = mm_allocator_new(1024);
-    }
+    // unsafe {
+    //     let alloc = mm_allocator_new(1024);
+    // }
     println!("Hello, world!");
 }
