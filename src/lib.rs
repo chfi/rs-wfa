ea5ade92dfc65e1ef5fae117934bee7cc7f3a0e8
Christian <christian@chfi.se>
2020-10-27 17:31:24 -0500

    Always use the automatically generated bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-// include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
 pub mod affine_wavefront;
 pub mod bindings;
 pub mod mm_allocator;
