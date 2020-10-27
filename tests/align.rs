d53e54e69fc5fa4ca6804ee29b3f05bc4be1575c
Christian <christian@chfi.se>
2020-10-26 20:20:42 -0500

    Test with texts shorter than used to allocate wavefront

--- tests/align.rs
+++ tests/align.rs
@@ -1,5 +1,14 @@
 use wfa_rs::{affine_wavefront::*, bindings::*, mm_allocator::*, penalties::*};
 
+fn new_complete<'a>(
+    alloc: &'a MMAllocator,
+    pattern_len: usize,
+    text_len: usize,
+    penalties: &mut AffinePenalties,
+) -> AffineWavefronts<'a> {
+    AffineWavefronts::new_complete(pattern_len, text_len, penalties, alloc)
+}
+
 fn run_complete<'a>(
     alloc: &'a MMAllocator,
     pattern_len: usize,
@@ -54,6 +63,35 @@ fn longer_texts() {
     run_complete(&alloc, 10, 10, &pattern, &text, &mut penalties);
 }
 
+#[test]
+fn shorter_texts() {
+    let alloc = MMAllocator::new(BUFFER_SIZE_8M as u64);
+
+    let pattern = String::from("TCTTTACTCGCGCGTTGGAGAAATACAATAGT");
+    let text = String::from("TCTATACTGCGCGTTTGGAGAAATAAAATAGT");
+
+    let mut penalties = AffinePenalties {
+        match_: 0,
+        mismatch: 4,
+        gap_opening: 6,
+        gap_extension: 2,
+    };
+
+    let mut wavefronts = new_complete(
+        &alloc,
+        pattern.len() + 10,
+        text.len() + 10,
+        &mut penalties,
+    );
+
+    wavefronts.align(pattern.as_bytes(), text.as_bytes());
+
+    let score = wavefronts.edit_cigar_score(&mut penalties);
+    assert_eq!(score, -24);
+
+    let cigar = wavefronts.cigar_bytes();
+    let cg_str = std::str::from_utf8(&cigar).unwrap();
+    assert_eq!("MMMXMMMMDMMMMMMMIMMMMMMMMMXMMMMMM", cg_str);
 }
 
 #[test]
